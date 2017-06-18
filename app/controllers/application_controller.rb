3c4e2081e91e76b6fca458689972b0b2457a9922
Robert Passmore <robertmpassmore@gmail.com>
2017-06-18 11:14:56 +0200

    finished static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def say_hello
-  	render html: "Hey ;)"
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/about'
-  get 'static_pages/help'
+  get 'static_pages/help'  
+  get 'static_pages/contact-us'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#say_hello'
+  root 'static_pages#home'
 end
