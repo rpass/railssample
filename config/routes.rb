16c10c8125dc67a8df7ef332cbe311e164f2731d
Rob Passmore <robertmpassmore@gmail.com>
2017-07-23 16:04:08 +0200

    ch 5 conclude

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contact-us', to: 'static_pages#contact_us'
+  get '/signup', to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
