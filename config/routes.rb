40337ab942cc0b14c121bf43a1be6072e1a28eb7
Rob Passmore <robertmpassmore@gmail.com>
2017-08-13 14:25:31 +0200

    add a page to show Users
    - UserController#show
    - show.html.erb
    - resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get '/contact-us', to: 'static_pages#contact_us'
   get '/signup', to: 'users#new'
 
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end
