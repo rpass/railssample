af63c5fd06c8eda920f678b9f20f034432e5c5d2
Rob Passmore <robertmpassmore@gmail.com>
2017-08-28 07:57:08 +0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20170805100832) do
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
