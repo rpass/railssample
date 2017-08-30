326f112968a550afd9a151554ba12dd8b8a2cefb
Rob Passmore <robertmpassmore@gmail.com>
2017-08-30 08:32:16 +0200

    add basic auth checking

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = 'welcome!'
+    else
+      # create error message
+      flash[:error] = 'you failed to login'
+      render 'new'
+    end
+  end
+
+  def destroy
   end
 end
