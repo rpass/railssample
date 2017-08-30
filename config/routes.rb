783fd7f9477669185b5f9800541df8cc57924649
Rob Passmore <robertmpassmore@gmail.com>
2017-08-30 08:12:51 +0200

    generate sessions controller and adjust for custom named routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
+  # login / logout
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
