7846a1c4a285ba58d18569d15855dd41b55ffdf0
Rob Passmore <robertmpassmore@gmail.com>
2017-09-09 12:32:32 +0200

    ch9 - add tests for remember_me functionality
    - notably empower the integration tests to peak into the user object

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in(user)
-      remember(user)
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in(@user)
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       flash[:success] = 'welcome!'
-      redirect_to user
+      redirect_to @user
     else
       # create error message
       flash.now[:danger] = 'you failed to login'
