a0f11aca18cddfb28b12c700c20f9d1587dc6151
Rob Passmore <robertmpassmore@gmail.com>
2017-09-09 11:29:38 +0200

    ch9 - allow for logout
    - can now forget users
    - fix small bug that caused a 500 when logging out twice

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,10 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(self.remember_token))
   end
 
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -16,19 +16,25 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   test "login changes layout followed by logout" do
+    # login successfully
     get login_path
     post login_path, params: { session: { email: @user.email, password: 'password' } }
     assert is_logged_in?
     assert_redirected_to @user
+
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
 
+    # logout successfully
     delete logout_path
     refute is_logged_in?
     assert_redirected_to root_url
+
+    # simulate clicking logout again
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
