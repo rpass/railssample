fdbd8e97c00b0ca6f91882d6542e9f8578f6f537
Rob Passmore <robertmpassmore@gmail.com>
2017-09-10 15:17:48 +0200

    ch10 - complete authorization and friendly forwarding
    - check for user is logged in for profile edit and update
    - check for correct user is logged in for profile edit and update
    - allow friendly forwarding for logged_out users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       log_in(@user)
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       flash[:success] = 'welcome!'
-      redirect_to @user
+      redirect_back_or @user
     else
       # create error message
       flash.now[:danger] = 'you failed to login'
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -3,9 +3,11 @@ require 'test_helper'
 class UsersEditTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: "",
@@ -16,9 +18,11 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert', "The form contains 4 errors."
   end
 
-  test "successful edit" do
+  test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    assert session[:forwarding_url].nil?
     name = "robert II"
     email = "royal-tea@gmail.com"
     patch user_path(@user), params: { user: { name: name,
@@ -30,5 +34,33 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    refute flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    refute flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect edit when logged in as the wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    refute flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as the wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    refute flash.empty?
+    assert_redirected_to root_url
   end
 end
