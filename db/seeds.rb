e96f3c477d8cb3d5861481d3b6527464766bbbc8
Rob Passmore <robertmpassmore@gmail.com>
2017-09-12 08:58:38 +0200

    ch10 - add pagination for users index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!({
+              name: "rob",
+              email: "rob@gmail.com",
+              password: 'password',
+              password_confirmation: 'password'
+            })
+
+99.times do |n|
+  name = Faker::Name.name
+  password = 'password'
+  email = "example-#{n+1}@gmail.com"
+
+  puts "seeding user #{n} - #{name}" if n%10 == 0
+  User.create!({
+                 name: name,
+                 email: email,
+                 password: password,
+               })
+end
