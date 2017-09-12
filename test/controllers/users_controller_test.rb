9034080262865664a6f794f1d25b0c74950f1174
Rob Passmore <robertmpassmore@gmail.com>
2017-09-12 06:52:30 +0200

    ch10 - add users index page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,6 +4,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
     get signup_path
     assert_response :success
-  end
+    end
 
+  test "should be redirected from index to login if not logged in" do
+    get users_path
+    assert_redirected_to login_path
+  end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -15,4 +15,16 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get contact_us_path
     assert_select "title", full_title('Contact Us')
   end
+
+  test "layout links when logged in" do
+    user = users(:michael)
+    log_in_as(user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", user_path(user)
+    assert_select "a[href=?]", edit_user_path(user)
+    assert_select "a[href=?]", logout_path
+  end
 end
