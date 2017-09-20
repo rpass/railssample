a8f18364974503aeeaea5dd787c7b5da61ccbec6
Rob Passmore <robertmpassmore@gmail.com>
2017-09-20 08:34:28 +0200

    ch13 - add validation to the Micropost model
    - user has to be present
    - content has to be present and a max of 140 characters

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    @micropost = Micropost.new({
+                                 user: @user,
+                                 content: "Lorem ipsum"
+                               })
+  end
+
+  test "micropost should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    refute @micropost.valid?
+  end
+
+  test "content should be present" do
+    @micropost.content = "   "
+    refute @micropost.valid?
+  end
+
+  test "content should be a maximum of 140 characters" do
+    @micropost.content = 'a'*141
+    refute @micropost.valid?
+
+    @micropost.content = 'a'*140
+    assert @micropost.valid?
+  end
 end
