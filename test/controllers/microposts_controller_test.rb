336c75a1762124bf09cfea2bb500f51008f19c34
Rob Passmore <robertmpassmore@gmail.com>
2017-09-22 08:27:55 +0200

    ch13 - first step in controlling micropost resources
    - move logged_in_user to the ApplicationController for use by all controllers
    - add routes for micropost create and destroy
    - add before_action to micropost controller to ensure user is signed in before allow create or delete of microposts

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @micropost = microposts(:orange)
+  end
+
+  test "should redirect create to login when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: "Lorem ipsum" } }
+    end
+    assert_redirected_to login_path
+  end
+
+  test "should redirect delete to login when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(@micropost.id)
+    end
+    assert_redirected_to login_path
+  end
 end
